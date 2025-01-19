09c1d714690e96e7c41e345942b38ec6c075b3db
rultor <me@rultor.com>
2025-01-19 13:24:24 +0000

    version set to 0.6.2

--- build.lua
+++ build.lua
@@ -30,7 +30,7 @@ cleanfiles = {"*.glo", "*.fls", "*.idx", "*.out", "*.fdb_latexmk", "*.aux", "*.c
 
 uploadconfig = {
   pkg = "yb-book",
-  version = "0.0.0",
+  version = "0.6.2",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
@@ -51,6 +51,6 @@ uploadconfig = {
 function update_tag(file, content, tagname, tagdate)
   return string.gsub(
     string.gsub(content, "0%.0%.0", tagname),
-    "0000/00/00", os.date("%Y/%m/%d")
+    "2025/01/19", os.date("%Y/%m/%d")
   )
 end
