a37dc2dea8af24914ddc357eb5ef915964d2ca08
Rultor.com <gpg@rultor.com>
2026-05-31 08:51:25 +0000

    version set to 0.11.0

--- build.lua
+++ build.lua
@@ -14,7 +14,7 @@ checkruns = 3
 
 uploadconfig = {
   pkg = "yb-book",
-  version = "0.0.0 0000-00-00",
+  version = "0.11.0 2026-05-31",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
